6b41af45951b1067055df3ae220f80bc0e66bbca
unknown <mennafikry63@gmail.com>
2024-02-25 01:22:14 +0200

    added controller

--- routes/cartroutes.js
+++ routes/cartroutes.js
@@ -12,19 +12,3 @@ router.delete('/',clearCart);
 module.exports = router;
 
 
-// const express = require("express")
-// const router = express.Router();
-// const { getAllCourses, getCourseById, createCourse,
-//     updateCourse, deleteCourse } = require('../controllers/courses.controller');
-
-// router.get("/",getAllCourses)
-
-// router.get("/:id",getCourseById )
-
-// router.post("/",createCourse)
- 
-// router.patch("/:id",updateCourse)
-
-// router.delete("/:id",deleteCourse)
-
-// module.exports = router;
