ebcc31595b91b85a9fd94786c92313b25122ec4d
unknown <mi1566924@gmail.com>
2024-02-25 12:39:40 +0200

    edit cart

--- routes/cartroutes.js
+++ routes/cartroutes.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const router = express.Router();
 const {getCart,addToCart,updateCartItem,removeCartItem,clearCart}=require('../controllers/cartController');
-
-router.get('/',getCart);
-router.post('/',addToCart);
-router.patch('/:productId',updateCartItem);
-router.delete('/:productId',removeCartItem);
-router.delete('/',clearCart);
+ const authController = require("./../controllers/authController")
+router.get('/',authController.protect,getCart);
+router.post('/',authController.protect,addToCart);
+router.patch('/:productId',authController.protect,updateCartItem);
+router.delete('/:productId',authController.protect,removeCartItem);
+router.delete('/',authController.protect,clearCart);
 
 
 module.exports = router;
