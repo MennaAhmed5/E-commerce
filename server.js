899766bccc1049ced90655496cc9ce5bf40f69cd
unknown <mi1566924@gmail.com>
2024-02-27 00:33:32 +0200

    resolve

--- server.js
+++ server.js
@@ -5,14 +5,10 @@ const app = express();
 const port = process.env.PORT||8000;
 
 const userRouter = require('./routes/userRouter');
-<<<<<<< HEAD
 const productRouter = require('./routes/productRouter');
 const cartRoute=require('./routes/cartroutes');
-=======
-const cartRoute = require('./routes/cartroutes')
 const paymentRoute = require('./routes/paymentRouter')
->>>>>>> 37e2c0a6011ac176cbf1cf74ef224daf7fc55660
-
+ 
  
 
 //Middlewars
@@ -21,14 +17,10 @@ app.use(express.json());
 
 //Routes
 app.use("/api/v1/users",userRouter);
-<<<<<<< HEAD
 app.use('/api/v1/cart',cartRoute);
 app.use('/api/v1/products',productRouter)
-=======
-app.use('/api/v1/cart', cartRoute)
 app.use('/api/v1/checkout', paymentRoute)
->>>>>>> 37e2c0a6011ac176cbf1cf74ef224daf7fc55660
-
+ 
 
 
 
