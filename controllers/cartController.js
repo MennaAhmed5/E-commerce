82d69a6ce70fe3ae490eb4e89a5ef95c56b9b470
unknown <mennafikry63@gmail.com>
2024-02-27 20:31:25 +0200

    m

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -35,68 +35,129 @@ const getCart=async (req , res)=>{
 
 //add product to cart
 
-const addToCart=async (req, res)=> {
-    const {productId,color,quantity} = req.body;
-    const product=  await Product.findById(productId);
+const addToCart = async (req, res) => {
+  const { productId, color } = req.body;
+  const product = await Product.findById(productId);
 
-    if(!product){
-      return res.json({message:"product not found!"})
-    }
-
-    //get cart for the logged user
-
-    const cart= await Cart.findOne({user:req.user._id});
-
-    if(!cart){
-     cart = await Cart.create({
-      user: req.user._id,
-      cartItems: [{ product: productId, color:color,quantity:quantity, price: product.price }],
-    });
-    }else{
-      //check if the product is already in the cart=>update quality
-      const  productCartIndex= Cart.cartItem.findIndex((item )=>item.product.toString()===productId&&item.color===color);
-      if (productCartIndex !==-1){
-        const cartitem=cart.cartItems[productCartIndex];
-        cartitem.quantity+=1;
-        cart.cartItems[productCartIndex]=cartitem;
-      }
-      else{
-        //if the product not in the cart u need to push it 
-
-        cart.cartItems.push({product:productId,color:color,quantity:quantity,price:product.price})
-      }
-    };
-    //
-    // const cart = new Cart(cartItem);
-    await cart.save();
-    res.status(200).json({ message: 'Product added to cart successfully', data: cart });
+  if (!product) {
+    return res.json({ message: "Product not found!" });
   }
 
+  // Get cart for the logged user
+  let cart = await Cart.findOne({ user: req.user._id });
+
+  if (!cart) {
+    cart = await Cart.create({
+      user: req.user._id,
+      cartItems: [{ product: productId, color, price: product.price }],
+    });
+
+    return res.json({ message: "Cart created", data: cart });
+  }
+
+  // Check if the product is already in the cart
+  const cartItem = cart.cartItems.find(item => item.product.toString() === productId && item.color === color);
+
+  if (cartItem) {
+    // Update product quantity
+    cartItem.quantity += 1;
+    await cart.save();
+    return res.json({ message: " quantity updated", data: cart });
+  }
+
+  // Product not in cart, push product to cartItems array
+  cart.cartItems.push({ product: productId, color, price: product.price });
+  await cart.save();
+
+  res.status(200).json({ message: "Product added to cart ", data: cart });
+};
+
+// const addToCart = async (req, res) => {
+//   const { productId, color } = req.body;
+//   const product = await Product.findById(productId);
+
+//   // 1) Get Cart for logged user
+//   let cart = await Cart.findOne({ user: req.user._id });
+
+//   if (!cart) {
+//     // create cart fot logged user with product
+//     cart = await Cart.create({
+//       user: req.user._id,
+//       cartItems: [{ product: productId, color, price: product.price }],
+//     });
+
+
+//     res.json({message:"cart created......."});
+//   } else {
+//     // product exist in cart, update product quantity
+//     const productIndex = cart.cartItems.findIndex(
+//       (item) => item.product.toString() === productId && item.color === color
+//     );
+    
+//     if (productIndex > -1) {
+//       const cartItem = cart.cartItems[productIndex];
+//       cartItem.quantity += 1;
+    
+//       cart.cartItems[productIndex] = cartItem; 
+//     } else {
+//       // product not exist in cart,  push product to cartItems array
+//       cart.cartItems.push({ product: productId, color, price: product.price });
+//       res.json({message:"producted pushed to cart...."})
+//     }
+   
+//   }
+//   Cart.save();
+//   res.json({message:"producted pushed to cart...."})
+
+// }
+const updateCartItem = async (req, res) => {
+  const { quantity, color } = req.body;
+  const productId = req.params.productId;
+  const cart = await Cart.findOne({ user: req.user._id });
+
+  if (!cart) {
+    return res.json({ message: "No cart for this user" });
+  }
+
+  const cartItem = cart.cartItems.find(item => item.product.toString() === productId);
+
+  if (!cartItem) {
+    return res.json({ message: "Product not found in cart" });
+  }
+
+  cartItem.quantity = quantity;
+  cartItem.color = color;
+
+  await cart.save();
+
+  res.json({ message: "Updated successfully", data: cart });
+};
 
 //
-  const updateCartItem = async (req, res) => {
+//   const updateCartItem = async (req, res) => {
     
-    const {quantity,color} = req.body;
-    const productId = req.params.productId;
-   const cart= await Cart.findOne({ user:req.user._id });
-   if(!cart){
+//     const {quantity,color} = req.body;
+//     const productId = req.params.productId;
+//    const cart= await Cart.findOne({ user:req.user._id });
+//    if(!cart){
 
-    res.json({message:"No cart for this user ........"})
+//     res.json({message:"No cart for this user ........"})
 
-   }
-   const cartIndex=cart.cartItems.findIndex((item)=>item._id.toString()=== productId);
-   if(cartIndex > -1){
-   const currentItem = cart.cartItems[cartIndex];
-   currentItem.quantity=quantity;
-   currentItem.color=color;
-   cart.cartItems[cartIndex]=currentItem;
-   }
-   else{
-    res.json({message:"there is no product matchs that id "})
-   }
-     await cart.save();
-    res.json({message:"updated success",data:cart})
-}
+//    }
+//    const product=cart.cartItems.findById(productId);
+//    if(product){
+//    product.quantity=quantity;
+//    product.color=color;
+
+//   //  currentItem.color=color;
+//   //  cart.cartItems[cartIndex]=currentItem;
+//    }
+//    else{
+//     res.json({message:"there is no product matchs that id "})
+//    }
+//      await cart.save();
+//     res.json({message:"updated success",data:cart})
+// }
   
 
 /////removeSpecificCartItem
@@ -187,79 +248,4 @@ module.exports = {
 
 
 
-// const courses = []
 
-// const getAllCourses = async (req,res)=>{
-
-//     // const courses = await Course.find();
-//     const courses = await findAllCourses();
-//     res.send(courses);
-// }
-
-// const getCourseById = async (req,res)=>{
-//     // res.send(req.params.id);
-//     //const course = courses.find((c)=>c.id === req.params.id);
-//     const {id} = req.params;
-//     const course = await Course.findOne({_id:id})
-//    if(!course){
-//     res.status(404).send("the course with given ID was not found");
-//     return;
-//    }
-//     res.send(course);
-// }
-// const createCourse = async (req,res)=>{
-//     const course =  await createCouse(value)
-//     res.send(course);
-//     // id:uuid()
-//     // const course = { courseName: req.body.courseName, 
-//     //     description: req.body.description, 
-//     // };
-//     // const {error, value} = validateNewCourse(req.body);
-//     // if(error)
-//     // {
-//     //     //bad request
-//     //     res.status(400).send({message:"Invalid form field.."})
-//     //     return;
-//     // }
-//     // courses.push(course);
-//     // const course = await Course.create(value)
-    
-// }
-
-// const updateCourse = async(req,res)=>{
-//     // looking up for the course
-//     // const course= courses.find((c)=> c.id ===  req.params.id);
-//     const {id} = req.params;
-//     const course = await Course.findOne({_id:id})
-//     if(!course){
-//         res.status(404).send("the course with given ID was not exist");
-//         return;
-//     }
-//     // course.name = req.body.name;
-//     await Course.updateOne({_id:id},req.body)
-//     const updatedCourse = await Course.findById(id);
-//     res.send(updatedCourse);
-// }
-
-// const deleteCourse = async (req,res)=>{
-//     // const course= courses.find((c)=> c.id ===  req.params.id);
-//     const {id} = req.params;
-//     const course = await Course.findOne({_id:id})
-//     if(!course){
-//         res.status(404).send("the course with given ID was not exist");
-//         return;
-//        }
-//     // const index = courses.indexOf(course);
-//     // courses.splice(index,1)[1];
-//     await Course.deleteOne({_id:id})
-//     // return;
-//     res.send(course);
-// }
-
-// module.exports = {
-//     getAllCourses,
-//     getCourseById,
-//     createCourse,
-//     updateCourse,
-//     deleteCourse
-// }
